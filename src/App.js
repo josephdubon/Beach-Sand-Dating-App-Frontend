6929383e549834f8c3240fafaabb60b498b1f1ca
Joseph Dubon <josephdubon@pm.me>
2022-02-23 15:03:23 -0800

    added header component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
 
+import Header from "./components/Header";
+
 function App() {
     return (<div className="App">
-        <h1>Beach Sand Dating App</h1>
+        <Header />
     </div>);
 }
 
