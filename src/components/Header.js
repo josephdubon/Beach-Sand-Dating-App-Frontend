b8ef86fdfbec7310b6d1c6393e7175bc6cd4de13
Joseph Dubon <josephdubon@pm.me>
2022-02-23 15:04:42 -0800

    created Header component with logo and icons

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import './Header.css'
+
+import PersonIcon from '@material-ui/icons/Person'
+import IconButton from '@material-ui/core/IconButton'
+import ForumIcon from '@material-ui/icons/Forum'
+
+import logo from '../logo-header-beach-sand.png'
+
+const Header = () => {
+    return (<div className="header">
+        <IconButton>
+            <PersonIcon fontSize={'large'} className={'header__icon'}/>
+        </IconButton>
+        <img src={logo} alt="header" className="header__logo"/>
+        <IconButton>
+            <ForumIcon fontSize={'large'} className={'header__icon'}/>
+        </IconButton>
+    </div>)
+}
+
+export default Header
