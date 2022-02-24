04753c4b85695a4f6e1cd914b6ead6e45ab556dd
Joseph Dubon <josephdubon@pm.me>
2022-02-23 20:41:49 -0800

    added API url to axios file

--- src/components/axios.js
+++ src/components/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'https://beach-sand-dating-app.herokuapp.com'
+    baseURL: 'https://beach-sand-dating-app.herokuapp.com/'
 })
 
 export default instance
